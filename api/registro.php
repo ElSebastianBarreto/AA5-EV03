c811eee1304d89512c26dc37619381a23d2a4211
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-08-08 11:59:20 -0500

    eliminacion de api no requerida

--- api/registro.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('../claseUsuario/usuario.class.php');
-
-    if($_SERVER['REQUEST_METHOD'] == 'POST' 
-        && isset($_GET['usuario']) && isset($_GET['nombre']) && isset($_GET['clave']) ){
-            Client::crearUsuario($_GET['usuario'], $_GET['nombre'], $_GET['clave']);
-        }
-
-?>
