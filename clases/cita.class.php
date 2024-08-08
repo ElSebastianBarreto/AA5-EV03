22f1c9f2dfa4f300dfa8f7ba002e1aafc91a9376
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-08-07 22:36:01 -0500

    eliminacion de funcion login de clase cita

--- clases/cita.class.php
+++ clases/cita.class.php
@@ -38,22 +38,5 @@
             }
         }
 
-        //funcion de login
-public static function login($cedula,$clave){    
-    $database = new Connection();
-    $conn = $database->getConnection();
-    $stmt = $conn->prepare('SELECT * FROM cliente WHERE cedula=:cedula AND clave=:clave');
-    $stmt->bindParam(':cedula', $cedula);
-    $stmt->bindParam(':clave', $clave);
-    //mostrar si es correcto o no el login
-    if ($stmt->execute() && $stmt->rowCount() > 0) {
-        $result = $stmt->fetch();
-        echo json_encode(["message" => "Login correcto", "cedula" => $result['cedula']]);
-        header('HTTP/1.1 200 OK');
-    } else {
-        echo json_encode(["message" => "la clave o usuario son incorrectos"]);
-        header('HTTP/1.1 401 Unauthorized');
-    }
-
-}
+    
     }
